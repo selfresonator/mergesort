b43b1d25cf0760371ff834092cb8ea3502ef6022
selfresonator <selfresonator@gmail.com>
2016-08-08 23:42:12 -0700

    Working out beginning of mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,7 @@
 var mergeSort = function(array) {
-  var result = [];
-  var workArr = array.splice(array.length/2, array.length);
-
+  var mid = Math.floor(array.length/2);
+  var left = array.slice(0,mid);
+  var right = array.slice(mid,array.length);
+  
   return result;
 };
